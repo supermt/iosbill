9bb1233c92d5a528ac08ac8be36b7820750f9136
supermt <18007092757@163.com>
2016-03-13 02:50:29 +0800

    Initial Commit

---
+++ Bill3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Bill3
+//
+//  Created by superMT on 16/3/13.
+//  Copyright © 2016年 superMT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
